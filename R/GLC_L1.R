13ff78b61aa89a1c36636aaa37b48f4b6afeb2be
Alex Tantos <atantos@gmail.com>
2018-12-10 00:18:50 +0200

    initial commit of everything

---
+++ R/GLC_L1.R
@@ -0,0 +1,9 @@
+#' @title L1 profiles in the Greek Learner Corpus [GLC]
+#'
+#' @description The dataset includes the L1s of 100 students whose language proficiency assessment tests recorded in the GLC.
+#' @name GLC_L1
+#' @docType data
+#' @format A vector with 100 entries of character values.
+#' @references \href{https://auth.clarin.gr/resources/browse/greek-learner-corpus-uam-corpus-tool-version-3/7d9c64c0c83311e6b526aa3fc6ebde2ceadb41214f994fb9bc568e74a6fe3edb/}{Visit and download} the GLC's site in Clarin:el.
+#' @keywords GLC datasets
+NULL
