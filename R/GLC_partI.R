32b0caab398aa75e592f2496ce0ec54ccfb44733
Alex Tantos <atantos@users.noreply.github.com>
2019-01-10 15:28:11 +0200

    Minor editing on GLC_partI
    values -> type

--- R/GLC_partI.R
+++ R/GLC_partI.R
@@ -3,7 +3,7 @@
 #' @description The vector includes the listening task scores of 100 students whose language proficiency assessment tests were recorded in the GLC.
 #' @name GLC_partI
 #' @docType data
-#' @format A vector with 100 entries of numeric values.
+#' @format A vector with 100 entries of numeric type.
 #' @references \href{https://auth.clarin.gr/resources/browse/greek-learner-corpus-uam-corpus-tool-version-3/7d9c64c0c83311e6b526aa3fc6ebde2ceadb41214f994fb9bc568e74a6fe3edb/}{Visit and download} the GLC's site in Clarin:el.
 #' @keywords GLC datasets
 NULL
