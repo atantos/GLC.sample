a60d61b9176495f9548855a420d910505f614866
Alex Tantos <atantos@users.noreply.github.com>
2019-01-10 15:26:32 +0200

    Edited the GLC_partIII
    Made the description precise. Clarified that the values are of numeric type.

--- R/GLC_partIII.R
+++ R/GLC_partIII.R
@@ -1,9 +1,9 @@
 #' @title Speaking task scores in language proficiency assessment tests of the Greek Learner Corpus [GLC]
 #'
-#' @description The dataset includes the speaking task scores of 100 students whose language proficiency assessment tests were recorded in the GLC.
+#' @description The vector includes the speaking task scores of 100 students whose language proficiency assessment tests were recorded in the GLC.
 #' @name GLC_partIII
 #' @docType data
-#' @format A vector with 100 entries of character values.
+#' @format A vector with 100 entries of numeric type.
 #' @references \href{https://auth.clarin.gr/resources/browse/greek-learner-corpus-uam-corpus-tool-version-3/7d9c64c0c83311e6b526aa3fc6ebde2ceadb41214f994fb9bc568e74a6fe3edb/}{Visit and download} the GLC's site in Clarin:el.
 #' @keywords GLC datasets
 NULL
